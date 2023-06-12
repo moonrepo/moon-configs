9ce46cc32839869e909f4b549a5a36fc3d2fe934
Miles Johnson <mileswjohnson@gmail.com>
2023-06-11 20:48:01 -0700

    new: Add prettier.

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  embeddedLanguageFormatting: "auto",
+  endOfLine: "lf",
+  printWidth: 100,
+  proseWrap: "always",
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+};
